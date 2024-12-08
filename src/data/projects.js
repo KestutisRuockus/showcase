0d4be6e435bead9b2e65b38217c10f93e9d9a788
KestutisRuockus <kestutisruockus@gmail.com>
2024-12-08 15:37:20 +0200

    FriendFeed project;

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,30 +1,68 @@
 // E-Shop demo images
-import eShop1 from '../assets/eshop/e-shop-1.png'
-import eShop2 from '../assets/eshop/e-shop-2.png'
-import eShop3 from '../assets/eshop/e-shop-3.png'
-import eShop4 from '../assets/eshop/e-shop-4.png'
+import friendfeed1 from "../assets/friendFeed/friendFeed1.png";
+import friendfeed2 from "../assets/friendFeed/friendFeed2.png";
+import friendfeed3 from "../assets/friendFeed/friendFeed3.png";
+import friendfeed4 from "../assets/friendFeed/friendFeed4.png";
+
+// E-Shop demo images
+import eShop1 from "../assets/eshop/e-shop-1.png";
+import eShop2 from "../assets/eshop/e-shop-2.png";
+import eShop3 from "../assets/eshop/e-shop-3.png";
+import eShop4 from "../assets/eshop/e-shop-4.png";
 
 // TMDB demo images
-import tmdb1 from '../assets/tmdb/TMDB-1.png'
-import tmdb2 from '../assets/tmdb/TMDB-2.png'
-import tmdb3 from '../assets/tmdb/TMDB-3.png'
-import tmdb4 from '../assets/tmdb/TMDB-4.png'
+import tmdb1 from "../assets/tmdb/TMDB-1.png";
+import tmdb2 from "../assets/tmdb/TMDB-2.png";
+import tmdb3 from "../assets/tmdb/TMDB-3.png";
+import tmdb4 from "../assets/tmdb/TMDB-4.png";
 
 // Infinite-Scroll demo images
-import scroll1 from '../assets/infiniteScroll/scroll-1.png'
-import scroll2 from '../assets/infiniteScroll/scroll-2.png'
-import scroll3 from '../assets/infiniteScroll/scroll-3.png'
-import scroll4 from '../assets/infiniteScroll/scroll-4.png'
+import scroll1 from "../assets/infiniteScroll/scroll-1.png";
+import scroll2 from "../assets/infiniteScroll/scroll-2.png";
+import scroll3 from "../assets/infiniteScroll/scroll-3.png";
+import scroll4 from "../assets/infiniteScroll/scroll-4.png";
 
 export const projectsData = [
-    {
+  {
+    name: "FriendFeed",
+    description:
+      "App to share posts of thoughts or photos to the society. STILL IN DEVELOPMENT PROCESS",
+    images: [friendfeed1, friendfeed2, friendfeed3, friendfeed4],
+    netlify: "https://friendfeed.netlify.app/",
+    github: "https://github.com/KestutisRuockus/FriendsFeed",
+    techStack: [
+      { name: "JS", icon: "fa-brands fa-js", color: "text-yellow-400" },
+      { name: "React", icon: "fa-brands fa-react", color: "text-blue-400" },
+      {
+        name: "Typescript",
+        icon: "",
+        color: "",
+        imgUrl:
+          "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg",
+      },
+      { name: "CSS", icon: "fa-brands fa-css3-alt", color: "text-blue-600" },
+      {
+        name: "Tailwind",
+        icon: "",
+        color: "",
+        imgUrl:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "Firebase",
+        icon: "",
+        color: "",
+        imgUrl: "https://www.vectorlogo.zone/logos/firebase/firebase-ar21.svg",
+      },
+    ],
+  },
+  {
     name: "Clothes E-Shop",
     description:
       "Explore the latest trends and timeless classics at StyleHub, where fashion meets convenience. Our e-shop offers a diverse range of clothing for men, women, and kids, including everything from chic casual wear to elegant formal attire. With easy navigation, detailed product descriptions, and high-quality images, finding your perfect outfit has never been easier. Enjoy a seamless shopping experience with secure payment options and fast, reliable shipping. Stay stylish, stay confident with StyleHub",
     images: [eShop1, eShop2, eShop3, eShop4],
     netlify: "https://clothes-e-shop.netlify.app/",
-    github:
-      "https://github.com/KestutisRuockus/clothes-e-shop",
+    github: "https://github.com/KestutisRuockus/clothes-e-shop",
     techStack: [
       { name: "JS", icon: "fa-brands fa-js", color: "text-yellow-400" },
       { name: "React", icon: "fa-brands fa-react", color: "text-blue-400" },
@@ -51,8 +89,7 @@ export const projectsData = [
       "The Movie Database (TMDB) is a community built movie and TV database. Every piece of data has been added by our amazing community dating back to 2008. TMDB's strong international focus and breadth of data is largely unmatched and something we're incredibly proud of. Put simply, we live and breathe community and that's precisely what makes us different.",
     images: [tmdb1, tmdb2, tmdb3, tmdb4],
     netlify: "https://tmdb-discovery.netlify.app/",
-    github:
-      "https://github.com/KestutisRuockus/TMDB",
+    github: "https://github.com/KestutisRuockus/TMDB",
     techStack: [
       { name: "React", icon: "fa-brands fa-react", color: "text-blue-400" },
       {
@@ -62,7 +99,13 @@ export const projectsData = [
         imgUrl:
           "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
       },
-      { name: "Rest-API", icon: "", color: "", imgUrl: "https://img.icons8.com/?size=100&id=55497&format=png&color=000000" },
+      {
+        name: "Rest-API",
+        icon: "",
+        color: "",
+        imgUrl:
+          "https://img.icons8.com/?size=100&id=55497&format=png&color=000000",
+      },
     ],
   },
   {
@@ -71,8 +114,7 @@ export const projectsData = [
       "Infinite Scroll, load more images when bottom of screen reached. Fetch people posted photos and mark which photos is yuor most favorites",
     images: [scroll1, scroll2, scroll3, scroll4],
     netlify: "https://infinite-scroll-react-typescript.netlify.app/",
-    github:
-      "https://github.com/KestutisRuockus/assignment",
+    github: "https://github.com/KestutisRuockus/assignment",
     techStack: [
       { name: "JS", icon: "fa-brands fa-js", color: "text-yellow-400" },
       { name: "React", icon: "fa-brands fa-react", color: "text-blue-400" },
@@ -84,7 +126,13 @@ export const projectsData = [
           "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg",
       },
       { name: "CSS", icon: "fa-brands fa-css3-alt", color: "text-blue-600" },
-      { name: "Rest-API", icon: "", color: "", imgUrl: "https://img.icons8.com/?size=100&id=55497&format=png&color=000000" },
+      {
+        name: "Rest-API",
+        icon: "",
+        color: "",
+        imgUrl:
+          "https://img.icons8.com/?size=100&id=55497&format=png&color=000000",
+      },
     ],
   },
-]
+];
